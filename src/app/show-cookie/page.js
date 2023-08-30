c44302f2924c2ea14038323e40b410c62eac045d
Shohanur-Shohan <shohanurshohan734@gmail.com>
2023-08-30 18:11:52 +0600

    setCookie

--- src/app/show-cookie/page.js
+++ src/app/show-cookie/page.js
@@ -1,23 +1,21 @@
 'use client'
 import Cookies from "js-cookie";
-import { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-import { StrictMode } from "react";
+
 
 export default function ShowCookie() {
 
 
-  //  add cookies
-  const SetCookie = () => {
-    Cookies.set("theme", "dark", {
-      expires: 7,
-      secure: true,
-      sameSite: "Strict",
+  // //  add cookies
+  // const SetCookie = () => {
+  //   Cookies.set("theme", "dark", {
+  //     expires: 7,
+  //     secure: true,
+  //     sameSite: "Strict",
 
-    });
-  };
+  //   });
+  // };
 
-
+  //get cookie
   const GetCookie = () => {
     // alert(Cookies.get("theme"));
     const theme = Cookies.get("theme")
@@ -28,7 +26,7 @@ export default function ShowCookie() {
   return (
     <div className="justify-around mt-10 text-center">
 
-      <button onClick={SetCookie} className="mt-20 mr-1 btn btn-neutral">Set Cookie</button>
+      {/* <button onClick={SetCookie} className="mt-20 mr-1 btn btn-neutral">Set Cookie</button> */}
       <button onClick={GetCookie} className="mt-20 ml-1 btn btn-neutral">Get Cookie</button>
     </div>
   );
--- src/components/SetCookie.jsx
+++ src/components/SetCookie.jsx
@@ -1,13 +1,25 @@
 'use client'
+import Cookies from "js-cookie";
 import Link from "next/link";
 
 
 export default function SetCookie() {
 
+
+  //  add cookies
+  const SetCookie = () => {
+    Cookies.set("theme", "dark", {
+      expires: 7,
+      secure: true,
+      sameSite: "Strict",
+
+    });
+  };
+
   return (
     <div className='h-screen text-center'>
       <Link href={'/show-cookie'}>
-            <button className="mt-20 btn btn-neutral">Cookie Page</button>
+            <button onClick={SetCookie} className="mt-20 btn btn-neutral">Set Cookie</button>
         </Link>
 
     </div>
